0b1b9cb4be6053c867e162f829ffd335852ccaf1
sujung <qortnwjd58@naver.com>
2024-06-21 12:31:34 +0900

    Feat: User API delete, password

--- backend/accountapp/urls.py
+++ backend/accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from accountapp.views import find_user, logout, signup
+from accountapp.views import change_password, delete_user, find_passwrod, find_user, logout, signup
 
 app_name = "accountapp"
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', logout, name='logout'),
     path('find/user/', find_user, name='find_user'),
+    path('find/password/', find_passwrod, name='find_passwrod'),
+    path('change/password/', change_password, name='change_password'),
+    path('/', delete_user, name='delete_user'),
 ]
--- backend/accountapp/views.py
+++ backend/accountapp/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +7,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from accountapp.models import CustomUser
 from accountapp.serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
 
 # 회원가입
 @api_view(['POST'])
@@ -17,13 +20,12 @@ def signup(request):
         return Response({"error": "패스워드는 10자 이상이어야 합니다."}, status=status.HTTP_400_BAD_REQUEST)
     
     if serializer.is_valid():
-
-        
         user = serializer.save()
         user.set_password(password)
         user.save()
         return Response(status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # 로그아웃
 @authentication_classes([JWTAuthentication])
@@ -41,10 +43,51 @@ def find_user(request):
     email = request.data.get('email')
     phone_number = request.data.get('phone_number')
 
-    if request.user.email == email and request.user.phone_number == phone_number:
-        context = {
-            'username': request.user.username
-        }
-        return Response(context, status=status.HTTP_200_OK)
-    return Response({"detail": "User not found or not authorized"}, status=status.HTTP_404_NOT_FOUND)
+    if not request.user.email == email:
+        return Response({"message": "이메일이 일치하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+    elif request.user.phone_number == phone_number:
+        return Response({"message": "핸드폰 번호가 일치하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+    context = {
+        'username': request.user.username
+    }
+    return Response(context, status=status.HTTP_200_OK)
 
+
+# 비밀번호 재설정 인증
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+@api_view(['POST'])
+def find_passwrod(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    phone_number = request.data.get('phone_number')
+
+    user = get_user_model().objects.get(username=username, email=email, phone_number=phone_number)
+    refresh = RefreshToken.for_user(user)
+    if user:
+        return Response({'redirect': '/password/find','token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+    return Response({"message": "사용자를 찾을수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+
+# 비밀번호 재설정
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+@api_view(['POST'])
+def change_password(request):
+    new_password = request.data.get('new_password')
+    user = request.user
+    user.set_password(new_password)
+    user.save()
+    return Response({"message": "비밀번호 변경 완료"}, status=status.HTTP_200_OK)
+
+
+# 유저 삭제
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+@api_view(['DELETE'])
+def delete_user(request, user_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    if user == request.user:
+        user.delete()
+        return Response({"message": "회원 탈퇴 되었습니다"}, status=status.HTTP_200_OK)
+    return Response({"message": "자기 자신의 계정만 삭제 가능합니다."}, status=status.HTTP_400_BAD_REQUEST)
