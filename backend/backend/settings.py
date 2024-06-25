d4ca0103bb33850b555176992d0a3b180e1dde19
sujung <qortnwjd58@naver.com>
2024-06-25 16:53:27 +0900

    Test: jenkins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,7 @@ from django.urls import reverse_lazy
 import environ
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
