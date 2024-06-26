6a310317b347a1355dd7a82dda302b7ef73f705f
sujung <qortnwjd58@naver.com>
2024-06-26 10:54:13 +0900

    Chore: debug test finish

--- scripts/backend_build.sh
+++ scripts/backend_build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd ./backend
+
 echo ">>> backend build start"
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-cd ./backend
-echo "Current directory: $(pwd)"
+
 docker build -t ${repository} .
 docker push ${repository}
 docker rmi ${repository}
+
 echo ">>> backend build finish"
--- scripts/frontend_build.sh
+++ scripts/frontend_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo '>>> frontend build start'
-echo "Current directory: $(pwd)"
 cd ./cc_front
-echo "Current directory: $(pwd)"
+echo '>>> frontend build start'
+
 npm install -g npm@10.7.0
 npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/material @emotion/react @emotion/styled @mui/icons-material
 npm install @mui/icons-material
 npm run build
+
 echo '>>> frontend build finish'
