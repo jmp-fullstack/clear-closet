0c89b41f8596fc871f545d403584ec36de6ce9bd
sujung <qortnwjd58@naver.com>
2024-07-02 11:22:30 +0900

    Chore: Add script react

--- scripts/frontend_build.sh
+++ scripts/frontend_build.sh
@@ -4,9 +4,9 @@ cd ./frontend
 echo '>>> frontend build start'
 
 npm install -g npm@10.7.0
-npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/material @emotion/react @emotion/styled @mui/icons-material
 npm install @mui/icons-material
+npm install react-draggable
 npm run build
 
 echo '>>> frontend build finish'
