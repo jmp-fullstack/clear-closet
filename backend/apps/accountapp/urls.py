9de757d4ac17e54d264b1a2ad684fd5b31e66250
sujung <qortnwjd58@naver.com>
2024-07-04 15:18:19 +0900

    Feat: Modify user delete, refresh

--- backend/apps/accountapp/urls.py
+++ backend/apps/accountapp/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from apps.accountapp.views import change_password, delete_user, find_password, find_user, login, logout, signup
+from apps.accountapp.views import CustomTokenRefreshView, change_password, delete_user, find_password, find_user, login, logout, signup
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('refresh/', CustomTokenRefreshView.as_view(), name='refresh'),
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('find/user/', find_user, name='find_user'),
     path('find/password/', find_password, name='find_password'),
     path('change/password/', change_password, name='change_password'),
-    path('<int:user_pk>/', delete_user, name='delete_user'),
+    path('', delete_user, name='delete_user'),
 ]
--- data_processing/data_pipleline.py
+++ data_processing/data_pipleline.py
@@ -1,12 +1,14 @@
-from secondwear_crawler import secondwear_crawler
-from musinsa_crawler import musinsa_crawler
-from ably_crawler import return_json_data, extract_goods_info, collect_ably_data, make_ably_dataframe, main_ably,ably_crawler
-import pymysql
-import traceback
+from data_crawling.secondwear_crawler import secondwear_crawler
+from data_crawling.musinsa_crawler import musinsa_crawler
+from data_crawling.ably_crawler import ably_crawler
 import pandas as pd
-from typing import Iterable
-from typing import Union
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
+# --------- 셋팅 ---------
+load_dotenv()
+engine = create_engine(f'mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:3306/{os.getenv("DB_DATABASE")}')
 
 categories = {
     '상의' : ['니트', '후드', '맨투맨','셔츠블라우스','긴소매티셔츠','반소매티셔츠','민소매티셔츠','카라티셔츠','베스트'],
@@ -16,7 +18,6 @@ categories = {
     '원피스' : ['미니원피스', '미디원피스', '투피스', '롱원피스', '점프수트'],
     '스커트' : ['미니스커트', '미디스커트', '롱스커트']
 }
-
 
 ably_base_urls = {
 
@@ -75,272 +76,84 @@ ably_base_urls = {
 
 }
 
-
-class MariaDB:
-    """
-    MariaDB
-    """
-
-    def __init__(self, db_config:dict, cursor_type="tuple") -> None:
-        """
-        생성자 메서드
-        인스턴스 생성 시 db_config를 전달받아 DB에 연결합니다.
-        
-        **db_config**
-            host=database host (localhost)
-            port=port (3306)
-            user=username (root)
-            password=password (1q2w3e)
-            database=database name (testdb)
-            charset=charcter encoding (utf8mb4)
-        """
-
-        db_config['port'] = int(db_config.get('port', '3306'))
-        self.DB = pymysql.connect(**db_config)
-
-        if cursor_type == 'dict':
-            self.cursor_type = pymysql.cursors.DictCursor
-        else:
-            self.cursor_type = None
-            
-        return
-    
-    def __del__(self):
-        """
-        인스턴스 소멸 시 DB 연결 해제
-        """
-        self.DB.close()
-
-    def select(self, column_qry:str, table:str, limit=None, offset=None, order_by=None, where_condition=[]) -> tuple:
-        """
-        Select
-
-        ex) 
-        column_qry = "*" : 모든열
-        column_qry = "id, name, email" : 열이름
-        table = "esg_project" : 테이블이름
-
-        """
-
-        sql_qr = "SELECT {0} FROM {1}".format(column_qry, table)
-        if order_by:
-            sql_qr += ' ORDER BY {}'.format(order_by)
-        if limit:
-            sql_qr += ' LIMIT {}'.format(limit)
-        if offset:
-            sql_qr += ' OFFSET {}'.format(offset)
-        if where_condition:
-            for i, (col, eq, val) in enumerate(where_condition):
-                is_equal = '=' if eq else '!='
-                is_multiple = ' AND' if i > 1 else ' WHERE'
-                sql_qr += f'{is_multiple} {col}{is_equal}{val}'
-
-
-        with self.DB.cursor() as cur:
-            cur.execute(sql_qr)
-            return cur.fetchall()
-        
-
-    def insert(self, table:str, columns: str, value: tuple) -> Union[int, bool]:
-        """
-        Insert Data
-        
-        example)
-        table = "Students"
-        columns = "name, email, phone"
-        values = ('이름', '이메일', '번호')
-        """
-
-        sql_qr = f"INSERT INTO {table}({columns}) " \
-                  "VALUES (" +','.join(["%s"]*len(value)) +")"
-        # args = values
-        
-        try:
-            with self.DB.cursor() as cur:
-                cur.execute(sql_qr, value)
-                self.DB.commit()
-            return cur.lastrowid
-        except:
-            traceback.print_exc()
-            self.DB.rollback()
-            return False
-    
-    # values는 list 형식으로 넣었음, args로 함
-    def insert_many(self, table: str, columns: str, values: list) -> bool:
-        """
-        Insert Many Datas
-        
-        example)
-        table = "테이블이름"
-        columns = "name, email, phone" 열이름들
-        values = [
-            ('hong gildong', 'hgd123@gmail.com', '01012345678'),
-            ...
-        ] 각 밸류 값들
-        """
-        num_columns = len(columns.split(','))
-        sql = f"INSERT INTO {table} ({columns}) VALUES (" + ','.join(["%s"] * num_columns) + ");"
-        try:
-            with self.DB.cursor() as cur:
-                cur.executemany(sql, values)
-                self.DB.commit()
-            return True
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            traceback.print_exc()
-            self.DB.rollback()
-            return False
-
-
-    def update(self, table:str, set_columns:list[str], set_values:list[str], where_column:str, where_value) -> bool:
-        """
-        Update
-        
-        example)
-        table = "Students"
-        set_column = ["name"]
-        set_value = ["jason"]
-        where_column = "id"
-        where_value = "1"
-        """
-
-        set_statement = ', '.join('{}="{}"'.format(sc, sv) for sc, sv in zip(set_columns, set_values))
-        sql = "UPDATE {0} " \
-            "SET {1} " \
-            "WHERE {2}={3};".format(table, set_statement, where_column, where_value)
-        try:
-            with self.DB.cursor() as cur:
-                cur.execute(sql)
-                self.DB.commit()
-            return True
-        except:
-            traceback.print_exc()
-            self.DB.rollback()
-            return False
-        
-
-    def truncate(self, table:str, forcing=True) -> bool:
-        """
-        truncate table
-        """
-        try:
-            with self.DB.cursor() as cursor:
-                cursor.execute(f'SET FOREIGN_KEY_CHECKS = {int(forcing)}; TRUNCATE TABLE {table};')
-                self.DB.commit()
-                return True
-        except:
-            return False
-        
-
-
+# --------- 수집 ---------
 # 무신사
 musinsa_df = musinsa_crawler(categories)
 musinsa_df['size'] = 'NaN'
 
-
 # 에이블리
 ably_df = ably_crawler(ably_base_urls)
 
-
 # 세컨드웨어
 secondwear_df = secondwear_crawler(categories)
-
-
 secondwear_df['connect_url'] = [f'https://www.hellomarket.com/item/{connect_url.split("/")[-1]}?viewLocation=search_result' for connect_url in secondwear_df['connect_url']]
 
-# 통합과정
 
-# 칼럼 이름 정렬
+
+# --------- 전처리 --------- 
 columns = sorted(musinsa_df.columns)
 
-# 각 데이터프레임의 칼럼 이름 정렬
 musinsa_df = musinsa_df[columns]
 ably_df = ably_df[columns]
 secondwear_df = secondwear_df[columns]
 
-# 데이터프레임 합치기
 all_df = pd.concat([musinsa_df, ably_df, secondwear_df], ignore_index=True)
 all_df['price'] = all_df['price'].astype(int)
 all_df['type'] = all_df['type'].astype(int)
 
-# 열 순서 정렬 및 결과 확인
 all_df = all_df[['top_category','bottom_category','title','price','connect_url','product_url','type','status','brand','color','size']]
+df = all_df.drop_duplicates()
+df = df.fillna({'color': '지정안함', 'size': '지정안함'})
 
 
-df_shuffled = all_df.sample(frac=1).reset_index(drop=True)
-df_sample = df_shuffled[:100]
+# ---------- SQL
+
+# relation
+category_query = """
+SELECT id AS category_id, top_category, bottom_category 
+FROM product_categoryapp_productcategory
+"""
+
+option_query = """
+SELECT id AS option_id, color, size 
+FROM product_optionapp_productoption
+"""
+category_df = pd.read_sql(category_query, engine)
+option_df = pd.read_sql(option_query, engine)
+
+df = df.merge(category_df, on=['top_category', 'bottom_category'], how='left')
+df = df.merge(option_df, on=['color', 'size'], how='left')
+
+product_data = df[['title', 'price', 'brand', 'connect_url', 'type', 'status', 'category_id', 'option_id']]
+product_data.columns = ['product_title', 'price', 'brand', 'connect_url', 'product_type', 'product_status', 'category_id', 'option_id']
 
 
+batch_size = 5000
+# product table
+for start in range(0, len(product_data), batch_size):
+    batch_df = product_data[start:start+batch_size]
+    batch_df.to_sql('productapp_product', engine, if_exists='append', index=False)
 
 
-db_config = {
-    'host' : 'localhost',
-    'port' : '3306',
-    'user' : 'closet',
-    'password' : 'Y7&hK!2zV9$pL3#x',
-    'database' : 'db_closet',
-    'charset' : 'utf8mb4',
-}
+# image table
+image_data = df[['product_url']].copy()
+image_data.columns = ['image_url']
+image_data['user_id'] = None
+
+for start in range(0, len(image_data), batch_size):
+    batch_df = image_data[start:start+batch_size]
+    batch_df.to_sql('imageapp_totalimage', engine, if_exists='append', index=False)
 
 
-conn = MariaDB(db_config)
+# product_image table
+with engine.connect() as conn:
+    product_df = pd.read_sql("SELECT id, connect_url FROM productapp_product", conn)
+    image_df = pd.read_sql("SELECT id, image_url FROM imageapp_totalimage", conn)
 
-# # product_category 테이블은 한번만 insert -> 주키 생성
-# table = 'product_category'
-# columns = 'top_category, bottom_category'
-# values = [category_composition for category_composition in set(zip(all_df['top_category'], all_df['bottom_category']))]
-# success = conn.insert_many(table, columns, values)
-# print("Insert successful:", success)
+product_image_df = pd.DataFrame({
+    'totalimage_id': image_df['id'].to_list(),
+    'product_id': product_df['id'].to_list()
+})
 
-
-# product_option 테이블을 먼저 넣기 -> 주키 생성
-table = 'product_option'
-columns = 'color, size'
-values = [option_composition for option_composition in set(zip(df_sample['color'], df_sample['size']))]
-success = conn.insert_many(table, columns, values)
-print("Insert successful:", success)
-
-
-
-option_list = []
-for (color, size) in zip(df_sample['color'], df_sample['size']) :
-    cur = conn.DB.cursor()
-    cur.execute("SELECT option_id FROM OPTION WHERE color=%s AND size=%s", (color, size))
-    option_id = cur.fetchone()[0]
-    option_list.append(option_list)
-
-category_list = []
-for (top_category, bottom_category) in zip(df_sample['top_category'], df_sample['bottom_category']) :
-    cur = conn.DB.cursor()
-    cur.execute("SELECT category_id FROM CATEGORY WHERE parent_category=%s AND child_category=%s", (top_category, bottom_category))
-    category_id = cur.fetchone()[0]
-    category_list.append(category_id)
-
-table = 'product'
-columns = 'category_id, option_id, title, price, connect_url, type, status, brand'
-values = [product_composition for product_composition in zip(category_list, option_list, df_sample['title'], df_sample['price'],df_sample['connect_url'], df_sample['type'],df_sample['status'], df_sample['brand'])]
-success = conn.insert_many(table, columns, values)
-print("Insert successful:", success)
-
-
-# 이미지 테이블 -> product_url 
-table = 'product_image'
-columns = 'user_id, product_url'
-values = [image_info for image_info in zip(['none']*len(df_sample),list(df_sample['product_url']))]
-success = conn.insert_many(table, columns, values)
-print("Insert successful:", success)
-
-
-# 데이터베이스 반영 및 연결 종료
-cur.close()
-conn.DB.commit()
-del conn
-
-  
-
-
-
-
-
-
-
+with engine.connect() as conn:
+    product_image_df.to_sql('imageapp_totalimage_product', conn, if_exists='append', index=False)
