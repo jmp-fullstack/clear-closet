dc80b08e203327b7fa27a944abf392fe85c0808b
sujung <qortnwjd58@naver.com>
2024-07-11 10:03:08 +0900

    Feat: Add profile isSell parmas API

--- backend/apps/articleapp/urls.py
+++ backend/apps/articleapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.articleapp.views import article_create, article_delete, article_detail, article_list, article_modify
+from apps.articleapp.views import article_create, article_delete, article_detail, article_is_sell, article_list, article_modify
 
 
 app_name = "articleapp"
@@ -11,4 +11,5 @@ urlpatterns = [
     path('detail/<int:article_pk>', article_detail, name="article_detail"),
     path('modify/<int:article_pk>', article_modify, name="article_modify"),
     path('delete/<int:article_pk>', article_delete, name="article_delete"),
+    path('isSell/<int:article_pk>', article_is_sell, name='article-isSell')
 ]
